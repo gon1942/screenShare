407835956f8330c32ba560b308423faf6b94f787
kg1942 <kg1942@gmail.com>
2023-04-21 15:29:26 +0900

    commit

---
+++ web_screen/app/src/main/java/com/ryan/screenshare/TurnServer.java
@@ -0,0 +1,10 @@
+package com.ryan.screenshare;
+
+import retrofit2.http.Header;
+import retrofit2.http.PUT;
+import retrofit2.Call;
+
+public interface TurnServer {
+    @PUT("/_turn/<xyrsys_channel>")
+    Call<TurnServerPojo> getIceCandidates(@Header("Authorization") String authkey);
+}
